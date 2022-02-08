3bfde012e9ab97667707087885e0a590ca8739fa
hogger <deuntlt@vip.qq.com>
2022-02-08 18:14:55 +0800

    [文件添加]使用elementUI

--- src/main.js
+++ src/main.js
@@ -10,5 +10,7 @@ import {
 } from 'vue'
 import App from './App.vue'
 import router from '../router/index'
+import installElementPlus from './plugins/element'
 const app = createApp(App)
+installElementPlus(app)
 app.use(router).mount('#app')
